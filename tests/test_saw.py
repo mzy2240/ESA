6f799a100c89825395b82453b5b57f809b6db26c
Brandon Thayer <blthayer@tamu.edu>
2019-11-01 11:52:28 -0500

    Moving test class around.

--- tests/test_saw.py
+++ tests/test_saw.py
@@ -107,6 +107,57 @@ class InitializationTestCase(unittest.TestCase):
 ########################################################################
 
 
+class ChangeAndConfirmParamsMultipleElementTestCase(unittest.TestCase):
+    """Test change_and_confirm_params_multiple_element."""
+
+    def test_success(self):
+        """Send in a simple command that matches what's already in the
+        case and ensure the output from PowerWorld matches.
+        """
+        # Create DataFrame which a) only modifies a couple elements
+        # (not all), b) does not actually change anything (e.g. matches
+        # the initial case values), and c) has NOT been cleaned (e.g.
+        # bad data types (string where float should be), not sorted by
+        # BusNum).
+        # Part a) ensures the merge + comparison in the method is
+        #   working.
+        # Part b) means we don't need to clean up after ourselves and
+        #   set things back.
+        # Part c) ensures the user doesn't have to provide a perfect
+        #   pre-cleaned DataFrame.
+        command_df = pd.DataFrame(
+            [[13, '1', 13.5, '5.8'],
+             [3, ' 1 ', '94.2', '19.0']],
+            columns=['BusNum', 'LoadID', 'LoadMW', 'LoadMVR']
+        )
+
+        # Patch the call to ChangeParametersMultipleElement.
+        with patch.object(saw_14, 'ChangeParametersMultipleElement'):
+            result = saw_14.change_and_confirm_params_multiple_element(
+                ObjectType='load', command_df=command_df)
+
+        self.assertIsNone(result)
+
+    def test_failure(self):
+        """Don't actually send in a command, and ensure that we get
+        an exception.
+        """
+        # Note this DataFrame is only one value off from what's in the
+        # base case model (130.5 in first row).
+        command_df = pd.DataFrame(
+            [[13, '1', 130.5, '5.8'],
+             [3, ' 1 ', '94.2', '19.0']],
+            columns=['BusNum', 'LoadID', 'LoadMW', 'LoadMVR']
+        )
+
+        # Patch the call to ChangeParametersMultipleElement.
+        with patch.object(saw_14, 'ChangeParametersMultipleElement'):
+            with self.assertRaisesRegex(CommandNotRespectedError,
+                                        'After calling .* not all parameters'):
+                saw_14.change_and_confirm_params_multiple_element(
+                    ObjectType='load', command_df=command_df)
+
+
 class GetKeyFieldsForObjectType(unittest.TestCase):
     """Test the get_key_fields_for_object_type method."""
 
@@ -638,57 +689,6 @@ class SolvePowerFlowTestCase(unittest.TestCase):
 ########################################################################
 # Private method tests
 ########################################################################
-
-
-class ChangeAndConfirmParamsMultipleElementTestCase(unittest.TestCase):
-    """Test change_and_confirm_params_multiple_element."""
-
-    def test_success(self):
-        """Send in a simple command that matches what's already in the
-        case and ensure the output from PowerWorld matches.
-        """
-        # Create DataFrame which a) only modifies a couple elements
-        # (not all), b) does not actually change anything (e.g. matches
-        # the initial case values), and c) has NOT been cleaned (e.g.
-        # bad data types (string where float should be), not sorted by
-        # BusNum).
-        # Part a) ensures the merge + comparison in the method is
-        #   working.
-        # Part b) means we don't need to clean up after ourselves and
-        #   set things back.
-        # Part c) ensures the user doesn't have to provide a perfect
-        #   pre-cleaned DataFrame.
-        command_df = pd.DataFrame(
-            [[13, '1', 13.5, '5.8'],
-             [3, ' 1 ', '94.2', '19.0']],
-            columns=['BusNum', 'LoadID', 'LoadMW', 'LoadMVR']
-        )
-
-        # Patch the call to ChangeParametersMultipleElement.
-        with patch.object(saw_14, 'ChangeParametersMultipleElement'):
-            result = saw_14.change_and_confirm_params_multiple_element(
-                ObjectType='load', command_df=command_df)
-
-        self.assertIsNone(result)
-
-    def test_failure(self):
-        """Don't actually send in a command, and ensure that we get
-        an exception.
-        """
-        # Note this DataFrame is only one value off from what's in the
-        # base case model (130.5 in first row).
-        command_df = pd.DataFrame(
-            [[13, '1', 130.5, '5.8'],
-             [3, ' 1 ', '94.2', '19.0']],
-            columns=['BusNum', 'LoadID', 'LoadMW', 'LoadMVR']
-        )
-
-        # Patch the call to ChangeParametersMultipleElement.
-        with patch.object(saw_14, 'ChangeParametersMultipleElement'):
-            with self.assertRaisesRegex(CommandNotRespectedError,
-                                        'After calling .* not all parameters'):
-                saw_14.change_and_confirm_params_multiple_element(
-                    ObjectType='load', command_df=command_df)
 
 
 class CleanDFOrSeriesTestCase(unittest.TestCase):
