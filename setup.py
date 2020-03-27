9672e60dac084bcc20c3c7df3769a64b8f8c5d82
Brandon Thayer <blthayer@tamu.edu>
2020-03-27 11:49:37 -0700

    First attempt at moving the version information to top-level VERSION file, and then reading that file anywhere the version is needed. Some pieces may be broken here.

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 import setuptools
+import os
 
-with open("ReadMe.md", "r") as fh:
+with open('VERSION', 'r') as fh:
+    __version__ = fh.read()
+
+with open("README.rst", "r") as fh:
     long_description = fh.read()
+
+# Append the version to __init__ before installing.
+with open(os.path.join('esa', '__init__.py'), 'a') as fh:
+    fh.write('__version__ = "{}"\n'.format(__version__))
 
 setuptools.setup(
     name='esa',
-    version='0.6.3',
+    version=__version__,
     description='Easy SimAuto (ESA): An easy-to-use Python connector to '
                 'PowerWorld Simulator Automation Server (SimAuto).',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     author='Zeyu Mao, Brandon Thayer, Yijing Liu',
     author_email='zeyumao2@tamu.edu, blthayer@tamu.edu, yiji21@tamu.edu',
     url='https://github.com/mzy2240/ESA',
@@ -40,5 +48,6 @@ setuptools.setup(
     install_requires=['pandas >= 0.24', 'numpy', 'pywin32', 'pypiwin32'],
     python_requires='>=3.5',
     license='MIT',
+    # TODO: Why aren't we zip safe?
     zip_safe=False
 )
