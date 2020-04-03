5baf3feb47a98eb96c8b83d8f6067b765a1945af
mzy2240 <mao.mzy@gmail.com>
2020-04-02 19:23:06 -0500

    1. Update the minimum version requirements for numpy and pandas, which addresses https://github.com/mzy2240/ESA/issues/40

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setuptools.setup(
               'PowerWorld Simulation Automation Server', 'SimAuto',
               'Automation', 'Power Systems', 'Electric Power', 'Power',
               'Easy SimAuto', 'ESA', 'Smart Grid', 'Numpy', 'Pandas'],
-    install_requires=['pandas >= 0.24', 'numpy', 'pywin32', 'pypiwin32'],
+    install_requires=['pandas >= 0.25', 'numpy >= 1.13.3', 'pywin32',
+                      'pypiwin32'],
     python_requires='>=3.5',
     # There are a couple tests that use networkx, and we use the magic
     # of sphinx for documentation.
