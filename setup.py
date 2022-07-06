b42de813ebdb2e3240cdbec49a4d2adf66f86f5d
Zeyu Mao <mao.mzy@gmail.com>
2022-07-06 00:00:05 -0500

    1. Update the doc deps

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setuptools.setup(
     # of sphinx for documentation. Coverage is necessary to keep the
     # coverage report up to date.
     extras_require={'test': ['networkx', 'coverage', 'matplotlib'],
-                    'doc': ['sphinx', 'tabulate'],
+                    'doc': ['sphinx', 'tabulate', 'sphinx_press_theme'],
                     'dev': ['pythran']},
     license='MIT',
     # TODO: Why aren't we zip safe?
