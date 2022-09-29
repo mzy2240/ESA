5065eb67ec908198109772fdb175017d1977fc7e
Zeyu Mao <mao.mzy@gmail.com>
2022-09-28 22:04:03 -0500

    1. Update the document

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -10,10 +10,25 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import inspect
 import os
 import sys
+from os.path import dirname, relpath
+from typing import Union
+from pathlib import Path
+import esa
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../'))
+
+# Get the project root dir, which is the parent dir of this
+doc_path = Path(__file__).parent
+project_root = doc_path.parent.parent
+src_root = project_root / "esa"
+
+# package data
+about = {}
+with open(src_root / "__about__.py") as fp:
+    exec(fp.read(), about)
 
 # Open and read the version file.
 with open('../../VERSION', 'r') as fh:
@@ -35,7 +50,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', "sphinx.ext.linkcode"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,6 +69,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'installation',
 
 # Include module's functions and class' methods
 # Note from Zeyu: This feature requires at least Sphinx version 5.2
+autoclass_content = 'both'
+autodoc_member_order = "bysource"
 toc_object_entries_show_parents = 'all'
 
 
@@ -81,4 +98,76 @@ html_static_path = ['_static']
 # Source: https://stackoverflow.com/a/9772922
 ########################################################################
 
-autoclass_content = 'both'
+def linkcode_resolve(
+    domain: str, info
+) -> Union[None, str]:  # NOQA: C901
+    """
+    Determine the URL corresponding to Python object
+    Notes
+    -----
+    From https://github.com/numpy/numpy/blob/v1.15.1/doc/source/conf.py, 7c49cfa
+    on Jul 31. License BSD-3. https://github.com/numpy/numpy/blob/v1.15.1/LICENSE.txt
+    """
+    if domain != "py":
+        return None
+
+    modname = info["module"]
+    fullname = info["fullname"]
+
+    submod = sys.modules.get(modname)
+    if submod is None:
+        return None
+
+    obj = submod
+    for part in fullname.split("."):
+        try:
+            obj = getattr(obj, part)
+        except Exception:
+            return None
+
+    # strip decorators, which would resolve to the source of the decorator
+    # possibly an upstream bug in getsourcefile, bpo-1764286
+    try:
+        unwrap = inspect.unwrap
+    except AttributeError:
+        pass
+    else:
+        if callable(obj):
+            obj = unwrap(obj)
+
+    try:
+        fn = inspect.getsourcefile(obj)
+    except Exception:
+        fn = None
+    if not fn:
+        return None
+
+    try:
+        source, lineno = inspect.getsourcelines(obj)
+    except Exception:
+        lineno = None
+
+    if lineno:
+        linespec = "#L%d-L%d" % (lineno, lineno + len(source) - 1)
+    else:
+        linespec = ""
+
+    fn = relpath(fn, start=dirname(esa.__file__))
+
+    if "dev" in about["__version__"]:
+        return "{}/blob/master/{}/{}/{}{}".format(
+            about["__github__"],
+            "src",
+            about["__package_name__"],
+            fn,
+            linespec,
+        )
+    else:
+        return "{}/blob/v{}/{}/{}/{}{}".format(
+            about["__github__"],
+            about["__version__"],
+            "src",
+            about["__package_name__"],
+            fn,
+            linespec,
+        )
---
+++ esa/__about__.py
@@ -0,0 +1,13 @@
+__title__ = "esa"
+__package_name__ = "esa"
+__description__ = "Easy SimAuto (ESA): An easy-to-use Python connector to PowerWorld Simulator " \
+                  "Automation Server (SimAuto)"
+__version__ = "1.3.2"
+__author__ = "Zeyu Mao"
+__github__ = "https://github.com/mzy2240/ESA"
+__docs__ = "https://mzy2240.github.io/ESA"
+__tracker__ = "https://github.com/mzy2240/ESA/issues"
+__pypi__ = "https://pypi.org/project/esa/"
+__email__ = "me@zeyumao.com"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2022 - Zeyu Mao"
